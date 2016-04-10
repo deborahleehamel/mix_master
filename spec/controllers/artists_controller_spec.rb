8b4da8d67f6e3f3a78cc7c7640d805454965e048
Deborah Hamel <deborahleehamel@gmail.com>
2016-04-10 03:48:22 -0600

    controller tests for POST #create invalid params

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -64,6 +64,20 @@ RSpec.describe ArtistsController, type: :controller do
         expect(response).to redirect_to(Artist.last)
       end
     end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved artist as @artist" do
+        post :create, {:artist => attributes_for(:artist, name: nil)}
+
+        expect(assigns(:artist)).to be_a_new(Artist)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:artist => attributes_for(:artist, name: nil)}
+
+        expect(response).to render_template("new")
+      end
+    end
   end
 
 end
